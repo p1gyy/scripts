e26c7c0e8c596b914e71531a53e7cfff621eeafc
Piggy Gaming <87405798+p1gyy@users.noreply.github.com>
2024-11-06 00:15:52 -0500

    Create spoofmacinstall_upstart.sh

---
+++ spoofmacinstall_upstart.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+INTERFACE="wlan0"
+NEW_MAC="C4:D0:E3:39:AF:8A"
+SCRIPT_PATH="/bin/spoofmac.sh"
+SERVICE_PATH="/etc/init/spoofmac.conf"
+
+echo "Creating script at $SCRIPT_PATH"
+cat <<EOF > "$SCRIPT_PATH"
+#!/bin/bash
+
+ip link set dev $INTERFACE down
+ip link set dev $INTERFACE address $NEW_MAC
+ip link set dev $INTERFACE up
+EOF
+
+chmod +x "$SCRIPT_PATH"
+
+echo "Creating Upstart service at $SERVICE_PATH"
+cat <<EOF > "$SERVICE_PATH"
+description "spoofs mac address on startup"
+start on network-services
+task
+
+script
+    "$SCRIPT_PATH"
+end script
+EOF
+
+initctl reload-configuration
+initctl start spoofmac
+
+echo "Service installed!"
+initctl status spoofmac
